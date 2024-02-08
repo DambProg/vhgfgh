54c31ef653f8b067733cc8a8f01b2a1acb4555b7
DambProg <159420308+DambProg@users.noreply.github.com>
2024-02-08 13:12:57 +0300

    Update sw.cs

--- sw.cs
+++ sw.cs
@@ -1,1 +1,14 @@
+using System;
+
+public class Main
+{
+	public Main(string[] args)
+	{
+		Console.WriteLine("Введите А:");
+		int a = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Введите B:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine((a+b) + "\n" + (a - b) + "\n" + (a / b) + "\n" + (a * b) + "\n")
+	}
+}
 
